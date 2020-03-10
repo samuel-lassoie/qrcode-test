e2607549600bf1a8b00b0280696d2809b40657ac
Samuel Lassoie <samuel.lassoie@manganelli.com>
2020-03-10 18:14:15 +0100

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  private users = [
+    {firstname: 'Samuel', lastname: 'Lassoie'},
+    {firstname: 'Roger', lastname: 'Lassoie'},
+    {firstname: 'Jeremy', lastname: 'Lassoie'},
+    {firstname: 'Toto', lastname: 'Lassoie'},
+    {firstname: 'Alex', lastname: 'TEst'},
+  ];
+
 
   constructor() {}
 
